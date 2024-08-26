3e508d319cab0800fb52b203e2f02bcff7c0c93e
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-08-26 12:54:34 +0300

    simple crud generator

---
+++ db.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const pg = require('pg');
+
+const pool = new pg.Pool({
+  user: 'marcus',
+  database: 'example',
+  password: 'marcus',
+  port: 5432,
+  host: '127.0.0.1',
+});
+
+module.exports = (table) => ({
+  query: async(sql, args) => await pool.query(sql, args),
+
+  create: async ({ ...data }) => {
+    const names = Object.keys(data);
+    const values = Object.values(data);
+    const numbers = new Array(names.length);
+    for (let i = 0; i < names.length; i++) numbers[i] = '$' + (i + 1);
+    const fields  = names.join(',');
+    const indices = numbers.join(',');
+    const sql = `INSERT INTO ${table} (${fields}) VALUES (${indices})`;
+    return await pool.query(sql, values);
+  },
+
+  read: async (id) => {
+    if (!id) return await pool.query(`SELECT * FROM ${table}`);
+    const sql = `SELECT * FROM ${table} WHERE id = $1`;
+    return await pool.query(sql, [id]);
+  },
+
+  update: async ({ ...data }) => {
+    const { id } = data;
+    delete data.id;
+    const keys = Object.keys(data);
+    const values = Object.values(data);
+    const { length } = keys;
+    const args = new Array(length);
+    for (let i = 0; i < length; i++) {
+      const arg = keys[i] + '=$' + (i + 1);
+      args[i] = arg;
+    }
+    const delta = args.join(',');
+    const sql = `UPDATE ${table} SET ${delta} WHERE id=$${length + 1}`;
+    values.push(id);
+    return await pool.query(sql, values);
+  },
+
+  delete: async ({ ...selectors }) => {
+    const args = [];
+    const values = [];
+    let index = 1;
+    for (const key in selectors) {
+      values.push(selectors[key]);
+      const arg = key + '=$' + index++;
+      args.push(arg);
+    }
+    const sql = `DELETE FROM ${table} WHERE ${args.join(' AND ')}`;
+    return await pool.query(sql, values);
+  },
+});
--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const pg = require('pg');
+const db = require('./db.js');
 const http = require('node:http');
-const crypto = require('node:crypto');
 
 const receiveArgs = async (request) => {
   const chunks = [];
@@ -11,52 +10,21 @@ const receiveArgs = async (request) => {
   return JSON.parse(string);
 };
 
-const hash = (password) => new Promise((resolve, reject) => {
-  const salt = crypto.randomBytes(16).toString('base64');
-  crypto.scrypt(password, salt, 64, (error, key) => {
-    if (error) reject(error);
-    else resolve(salt + ':' + key.toString('base64'));
-  });
-});
-
-const pool = new pg.Pool({
-  user: 'marcus',
-  database: 'example',
-  password: 'marcus',
-  port: 5432,
-  host: '127.0.0.1',
-});
-
 const PORT = 8000;
 
 const routing = {
-  users: {
-    get: async (id) => {
-      if (!id) return await pool.query('SELECT id, login FROM users');
-      const sql = 'SELECT id, login FROM users WHERE id = $1';
-      return await pool.query(sql, [id]);
-    },
-    post: async ({ login, password }) => {
-      const passwordHash = await hash(password);
-      const sql = 'INSERT INTO users (login, password) VALUES ($1, $2)';
-      return await pool.query(sql, [login, passwordHash]);
-    },
-    put: async ({ id, login, password }) => {
-      const passwordHash = await hash(password);
-      const sql = 'UPDATE users SET login = $1, password = $2 WHERE id = $3';
-      return await pool.query(sql, [login, passwordHash, id]);
-    },
-    delete: async ({ id }) => {
-      const sql = 'DELETE FROM users WHERE id = $1';
-      return await pool.query(sql, [id]);
-    },
-  },
-};
+  users: db('users'),
+  country: db('country'),
+  city: db('city')
+}
+
+const crud = { get: 'read', post: 'create', put: 'update', delete: 'delete' };
 
 http.createServer(async (request, response) => {
   const { method, url } = request;
   const [name, ...args] = url.slice(1).split('/');
-  const handler = routing[name]?.[method.toLowerCase()];
+  const procedure = crud[method.toLowerCase()];
+  const handler = routing[name]?.[procedure];
   if (!handler) return void response.end('Not found');
   const source = handler.toString();
   const signature = source.slice(0, source.indexOf(')') + 1);
